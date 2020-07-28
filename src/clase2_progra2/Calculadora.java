2a52d2bcc53dfed7c981aacf5cc5b86d1ae5553b
KevinDevGT <37258552+KevinDevGT@users.noreply.github.com>
2020-07-27 18:22:44 -0600

    clase III

---
+++ src/clase2_progra2/Calculadora.java
@@ -0,0 +1,19 @@
+
+package clase2_progra2;
+
+public class Calculadora {//name class
+    
+    public void mensaje(){//metodo void
+        
+    }
+    
+    public int sumar(){//metodo int
+    
+    return 0;
+    }
+    
+    public String menajero(){
+    
+    return "Holis";
+    }
+}
---
+++ src/clase2_progra2/Clase2_progra2.java
@@ -0,0 +1,14 @@
+package clase2_progra2;
+
+public class Clase2_progra2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      Calculadora calculadora = new Calculadora();
+      System.out.println(calculadora.menajero());
+    }
+    
+}
